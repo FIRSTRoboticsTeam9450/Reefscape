9967861933fa92188ca6b6a65e139224d505057f
curtsGitHub <niuniucat99@gmail.com>
2025-01-27 14:25:55 -0800

    Added Scoring command

--- src/main/java/frc/robot/commands/ScoringCommand.java
+++ src/main/java/frc/robot/commands/ScoringCommand.java
@@ -1,25 +1,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ScoringPos;
 import frc.robot.subsystems.DualIntakeSubsystem;
+import frc.robot.subsystems.ElbowSubsystem;
 
 public class ScoringCommand extends Command{ // DONT USE
-    private double elevatorPos;
-    private double elbowPos;
-    private double pitchPos;
-    private double rollPos;
     private DualIntakeSubsystem intake = DualIntakeSubsystem.getInstance();
-    
-    public ScoringCommand(double elevatorPos, double elbowPos, double pitchPos, double rollPos) {
-        this.elevatorPos = elevatorPos;
-        this.elbowPos = elbowPos;
-        this.pitchPos = pitchPos;
-        this.rollPos = rollPos;
+    private ElbowSubsystem elbow = new ElbowSubsystem();
+    public ScoringCommand() {
     }
 
     @Override
     public void execute() {
-        ElbowCommand elbow = new ElbowCommand(elbowPos+1);
-        intake.setVoltage(-1);
+        CoordTestingCommand score = new CoordTestingCommand(ScoringPos.SCORE_CORAL);
+        if(elbow.atSetpoint()) {
+            intake.setVoltage(-1);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+
     }
 }
