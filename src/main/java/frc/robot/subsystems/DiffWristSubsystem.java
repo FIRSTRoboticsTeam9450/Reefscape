8ca94e773f8dd9539fb4f3b04e9917b5dac62d1b
KCheck25 <91163059+KCheck25@users.noreply.github.com>
2025-08-12 11:10:48 -0700

    Got the drive constants

--- src/main/java/frc/robot/subsystems/DiffWristSubsystem.java
+++ src/main/java/frc/robot/subsystems/DiffWristSubsystem.java
@@ -77,7 +77,7 @@ public class DiffWristSubsystem extends SubsystemBase {
         CANcoderConfiguration cc_cfg = new CANcoderConfiguration();
         cc_cfg.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5;
         cc_cfg.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
-        cc_cfg.MagnetSensor.MagnetOffset = 0.3984375;
+        cc_cfg.MagnetSensor.MagnetOffset = -0.39111328125;
         rollEncoder.getConfigurator().apply(cc_cfg);
         cc_cfg.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.2;
         cc_cfg.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
