fb663a5ca3260fb21dae686b4790c9ead9595da5
inky01925 <kitzanryan@gmail.com>
2025-10-09 15:35:18 -0700

    rezero roll

--- src/main/java/frc/robot/subsystems/DiffWristSubsystem.java
+++ src/main/java/frc/robot/subsystems/DiffWristSubsystem.java
@@ -78,7 +78,7 @@ public class DiffWristSubsystem extends SubsystemBase {
         CANcoderConfiguration cc_cfg = new CANcoderConfiguration();
         cc_cfg.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5;
         cc_cfg.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
-        cc_cfg.MagnetSensor.MagnetOffset = -0.158203125;
+        cc_cfg.MagnetSensor.MagnetOffset = -0.157958984375;
         rollEncoder.getConfigurator().apply(cc_cfg);
         cc_cfg.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.2;
         cc_cfg.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
@@ -231,7 +231,7 @@ public class DiffWristSubsystem extends SubsystemBase {
     public boolean atRollSetpoint() {
         double rollAngle = getRollAngle();
         double rollSetpoint = getRollSetpoint();
-        if ((rollAngle > rollSetpoint - 10) && (rollAngle < rollSetpoint + 10)) {
+        if ((rollAngle > rollSetpoint - 10) && (rollAngle < rollSetpoint + 10)) { // used to be 10
             return true;
         }
         return false;
